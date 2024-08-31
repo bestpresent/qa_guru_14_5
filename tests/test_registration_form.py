76ccaefadbbd695861082a109789962056670bc2
Nataly Isaeva <mamadany25@gmail.com>
2024-08-31 10:01:39 +0300

    passed

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,6 +1,7 @@
-
+import driver
 from selene import browser, command, have
 import os
+
 
 
 def test_form():
@@ -14,15 +15,15 @@ def test_form():
     browser.element('.react-datepicker__month-select').click().element('[value="2"]').click()
     browser.element('.react-datepicker__year-select').click().element('[value="1990"]').click()
     browser.element('.react-datepicker__day--031').click()
-    browser.element('#subjectsInput').type('life').press_enter()
+    browser.element('#subjectsInput').type('Comp').press_enter()
+    browser.element('#subjectsInput').type('eco').press_enter()
     browser.element('[for="hobbies-checkbox-2"]').with_(timeout=15).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('../1.jpg'))
     browser.element('#currentAddress').type('London').press_enter()
-    browser.element("#react-select-3-input").press_enter()
-    browser.element("#react-select-4-input").press_enter()
+    browser.element('#react-select-3-input').type("Haryana").press_enter()
+    browser.element('#react-select-4-input').type("Panipat").press_enter()
     browser.element('#submit').press_enter()
     browser.element("#example-modal-sizes-title-lg").should(have.text('Thanks for submitting the form'))
-
     browser.element('.table').all('td').even.should(
-        have.exact_texts('Isaeva Nataly', 'lady.astashina@yandex.ru', 'Female', '9123006789', '31 March,1990', 'life',
-                         'Music', '1.jpg', 'London', 'NCR Delhi'))
+        have.exact_texts('Nataly Isaeva', 'lady.astashina@yandex.ru', 'Female', '9123006789', '31 March,1990',
+                          'Computer Science, Economics', 'Reading', '1.jpg', 'London', 'Haryana Panipat'))
